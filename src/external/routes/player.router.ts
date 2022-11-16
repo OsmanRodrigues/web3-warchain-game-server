19e0785476b33c266cda07af733338bcbaae7840
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-11-16 16:50:15 -0300

    custom express server implemented

--- src/external/routes/player.router.ts
+++
@@ -1,5 +0,0 @@
-import {PlayerController} from '@adapters/controllers/player.controller'
-
-export class PlayerRoute {
-    constructor(private controller: PlayerController) {}
-}
--- src/external/web/server.ts
+++ src/external/web/server.ts
@@ -1,9 +1,28 @@
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
+import {json} from 'body-parser'
 import {ApolloServer} from '@apollo/server'
-import {startStandaloneServer} from '@apollo/server/standalone'
+import {ApolloServerPluginDrainHttpServer} from '@apollo/server/plugin/drainHttpServer'
+import {expressMiddleware} from '@apollo/server/express4'
 import {typeDefs, resolvers} from './graphql/schemas'
+import {route} from '../routes/route'
 
-const server = new ApolloServer({typeDefs, resolvers})
+const app = express()
+const httpServer = http.createServer(app)
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({httpServer})],
+})
 export async function serverHandler() {
-    const {url} = await startStandaloneServer(server, {listen: {port: 4040}})
-    console.log(`🚀  Server ready at ${url}`)
+    await server.start()
+    app.use(
+        route.main,
+        cors<cors.CorsRequest>(),
+        json(),
+        expressMiddleware(server),
+    )
+    await new Promise<void>(resolve => httpServer.listen({port: 4040}, resolve))
+    console.log(`🚀  Server ready at http://localhost:4040/graphql`)
 }
