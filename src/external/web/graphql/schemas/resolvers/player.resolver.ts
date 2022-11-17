b55b32fe4af543a7b191fa5f465f47a3cd665ab1
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-11-17 16:47:03 -0300

    chore: player controller incremented and player mutations exposed

--- src/external/web/graphql/schemas/resolvers/player.resolver.ts
+++ src/external/web/graphql/schemas/resolvers/player.resolver.ts
@@ -14,4 +14,14 @@ export const playerMutation = {
             playerController.createPlayer(player)
             return {info: 'Created', code: HttpSuccessCode.Created}
         }),
+    authPlayer: (_: unknown, player: PlayerDTO) =>
+        resolverMutationMiddleware(() => {
+            playerController.authPlayer(player)
+            return {info: 'Ok', code: HttpSuccessCode.Ok}
+        }),
+    removePlayer: (_: unknown, player: PlayerDTO) =>
+        resolverMutationMiddleware(() => {
+            const result = playerController.removePlayer(player.username)
+            return result
+        }),
 }
